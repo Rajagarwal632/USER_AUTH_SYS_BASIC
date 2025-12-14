c3011e64e1ab66d1e7a3f0aa6870f5be50f9a921
Rajagarwal632 <raj390504@gmail.com>
2025-12-14 12:25:44 +0530

    user_signin endpoint done

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const { Router } = require("express")
 const userrouter = Router()
 const bcrypt = require("bcrypt")
 const saltround = 10
+const mongoose = require("mongoogse");
 
 userrouter.post("/signup" , async function(req,res){
     const reqbody = z.reqbody({
@@ -29,7 +30,16 @@ userrouter.post("/signup" , async function(req,res){
 
     const hashpass = await bcrypt.hash(password,saltround)
 
-    const user = await usermodel.create({
+    const existuser = usermodel.findOne({
+        email
+    })
+    if(existuser){
+    res.status(403).json({
+        msg : "USER EXISTS"
+    })
+    return
+    }
+    await usermodel.create({
         email, 
         password : hashpass,
         name,
@@ -39,6 +49,33 @@ userrouter.post("/signup" , async function(req,res){
 })
 
 userrouter.post("/signin" , async function(req,res){
+   
+    const email = req.body.email
+    const password = req.body.password
+
+    const existuser = usermodel.findOne({
+        email
+    })
+    if(!existuser){
+        res.json({
+            msg : "USER NOT EXISTS"
+        })
+        return
+    }
+    const pass_match = bcrypt.compare(password,existuser.password)
+    if(pass_match){
+        const token = jwt.sign({
+            id : existuser._id.toString()
+        },JWT_USER_PASS)
+        res.json({
+            token : token
+        })
+    }else{
+        res.json({
+            msg : "INCORRECT PASSWORD"
+        })
+    }
+    
 
 })
 
